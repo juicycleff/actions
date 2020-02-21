424b4fb96e755ebcd1e5bdf2af8c202646e3973f
Ben Toogood <ben@micro.mu>
2020-02-21 13:47:59 +0000

    first commit

---
+++ changed-services/src/main.ts
@@ -0,0 +1,97 @@
+import * as core from '@actions/core';
+import * as github from '@actions/github';
+
+class File {
+  added: string = ''
+  modified: string = ''
+  removed: string = ''
+  filename: string = ''
+  status: string = ''
+  previous_filename: string = ''
+  distinct: boolean = true
+}
+
+async function getChangedPRFiles(client: github.GitHub, prNumber: number): Promise<Array<File>> {
+  const options = client.pulls.listFiles.endpoint.merge({
+    owner: github.context.repo.owner,
+    repo: github.context.repo.repo,
+    pull_number: prNumber
+  })
+
+  return client.paginate(options)
+}
+
+async function getChangedPushFiles(client: github.GitHub, base: string, head: string): Promise<Array<File>> {
+  return new Promise(async (resolve, reject) => {
+    const options = {
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo,
+      base,
+      head
+    }
+
+    client.repos.compareCommits(options)
+      .then((res: any) => resolve(res.data.files))
+      .catch(reject)
+  })
+}
+
+function pathForFile(f: File): string {
+  switch(f.status) {
+    case 'added':
+      return f.added || f.filename;
+    case 'modified':
+      return f.modified || f.filename;
+    case 'removed':
+      return f.removed || f.filename;
+    default:
+      return f.filename;
+  }
+}
+
+function directoryForPath(path: string): string | null {
+  const comps = path.split('/')
+  return comps.length > 1 ? comps[0] : null;
+}
+
+async function run(): Promise<void> {
+  try {
+    const context: any = github.client;
+    const token: string = core.getInput('githubToken');
+    const client = new context.GitHub(token);
+  
+    let files: Array<File>;
+    switch(context.eventName) {
+      case 'push':
+        files = await getChangedPushFiles(client, github.payload.before, github.payload.after);
+        break;
+
+      case 'pull_request':        
+        const { pull_request } = github.context.payload
+        if (!pull_request) {
+          core.setFailed('Could not get pull request from context, exiting')
+          return
+        }
+        files = await getChangedPRFiles(client, pull_request.number);
+        break;
+
+      default:
+        core.setFailed('Change not initiated by a PR or Push');
+        return;
+    }
+
+    const paths = files.map(pathForFile)
+    core.debug("Files: " + paths.join(", "))
+
+    const dirs = paths.map(directoryForPath).filter((value, index, self) => {
+      return value && self.indexOf(value) === index;
+    })
+
+    core.debug("Directories: " + dirs.join(", "))
+    core.setOutput('directories', dirs.join(","))
+  } catch (error) {
+    core.setFailed(error.message)
+  }
+}
+
+run()
